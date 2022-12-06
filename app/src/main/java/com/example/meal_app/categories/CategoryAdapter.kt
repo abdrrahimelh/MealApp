48821fb37d56e54327f79124b00daa1395c01a99
EL HIMIR ABDERRAHIM <59322515+abdrrahimelh@users.noreply.github.com>
2022-12-06 15:01:05 +0100

    Add Intent

--- app/src/main/java/com/example/meal_app/categories/CategoryAdapter.kt
+++ app/src/main/java/com/example/meal_app/categories/CategoryAdapter.kt
@@ -1,15 +1,18 @@
 package com.example.meal_app.categories
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.meal_app.MainActivity2
 import com.example.meal_app.R
 
 
@@ -18,8 +21,6 @@ class CategoryViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
     var nameTextView: TextView = itemView.findViewById(R.id.textview_name)
     var categoryImage: ImageView = itemView.findViewById(R.id.category_image)
     var like_button: ImageView = itemView.findViewById(R.id.like_category_image)
-    var myUriString : String ="drawable/heart_on.png"
-    val uri = myUriString.toUri()
 }
 
 class CategoryAdapter(private val context: Context,val categories: List<Category>): RecyclerView.Adapter<CategoryViewHolder>() {
@@ -37,9 +38,15 @@ class CategoryAdapter(private val context: Context,val categories: List<Category
                .into(categoryImage)
 
        }
+        holder.categoryImage.setOnClickListener{
+            val intent = Intent(context,MainActivity2::class.java)
+            intent.putExtra("category",categories.get(position).strCategory)
+            context.startActivity(intent)
+        }
            holder.like_button.setOnClickListener {
                holder.like_button.setImageResource(R.drawable.heart_on)
            }
+
     }
 
     override fun getItemCount(): Int {
