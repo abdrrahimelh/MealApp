71897a81459cb5c5a8588ea42892f6d9f73c0f4e
ghamdouni001 <ghofrane_hamdouni@DESKTOP-G4MN9G7.localdomain>
2022-12-29 23:14:36 +0100

    Add background and another relative layouts to the description page

--- app/src/main/java/com/example/meal_app/MainActivity3.kt
+++ app/src/main/java/com/example/meal_app/MainActivity3.kt
@@ -1,9 +1,12 @@
 package com.example.meal_app
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.meal_app.description.DescriptionResponse
@@ -18,14 +21,19 @@ class MainActivity3 : AppCompatActivity() {
     private  lateinit var textYoutube: TextView
     private  lateinit var textViewCategory: TextView
     private  lateinit var textViewRecipe: TextView
+    private lateinit var Rl : RelativeLayout
+    private lateinit var RlYoutube : RelativeLayout
     private lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         val idMeal = intent.getStringExtra("idMeal")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+        Rl = findViewById(R.id.relativelayoutInstructions);
+        RlYoutube = findViewById(R.id.YoutubeLayout);
         textViewInstructions = findViewById(R.id.Instructions)
         textViewInstructions.setMovementMethod(ScrollingMovementMethod())
         textYoutube = findViewById(R.id.URL)
+        textYoutube.setMovementMethod(LinkMovementMethod.getInstance());
         textViewCategory= findViewById(R.id.Category)
         textViewRecipe = findViewById(R.id.Recipe)
 
@@ -50,13 +58,27 @@ class MainActivity3 : AppCompatActivity() {
                     val descriptionResponse = gson.fromJson(it, DescriptionResponse::class.java)
                     Log.d("OKHTTP", "Got " + descriptionResponse.description?.count() +  " results")
                     Log.d("OKHTTP", "Got " + descriptionResponse.description?.get(0)?.strCategory +  " results")
-
+                    val instruction = descriptionResponse.description?.get(0)?.strInstructions
+                    val len = instruction?.length
+                    val lines: List<String>? = instruction?.split("\n")
+                    val count = lines?.count()!!
                     runOnUiThread {
+                        if (len != null ) {
+                           for (i in 0..count-1)
+                           Rl.getLayoutParams().height = len + lines[i].length
 
-                        textViewInstructions.text  = descriptionResponse.description?.get(0)?.strInstructions;
-                        textYoutube.text = descriptionResponse.description?.get(0)?.strYoutube;
+                        }
+                        textViewInstructions.text  = "STEP BY STEP\n\n\n" + descriptionResponse.description?.get(0)?.strInstructions;
                         textViewCategory.text = descriptionResponse.description?.get(0)?.strCategory;
                         textViewRecipe.text = descriptionResponse.description?.get(0)?.strMeal;
+                            if( descriptionResponse.description?.get(0)?.strYoutube != null && !(descriptionResponse.description?.get(0)?.strYoutube.equals("")) )
+                            {
+                                textYoutube.text = descriptionResponse.description?.get(0)?.strYoutube;
+
+                            }
+                        else {
+                                RlYoutube.setVisibility( View.GONE);
+                            }
 
 
                     }
